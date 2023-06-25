7c92ceac4165fd7061710a97c9b07dfee489d369
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-06-25 16:44:29 +0100

    Create chirps

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * A user has many chirps.
+     */
+    public function chirps()
+    {
+        return $this->hasMany(Chirp::class);
+    }
 }
